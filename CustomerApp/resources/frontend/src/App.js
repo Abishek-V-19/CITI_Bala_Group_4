94d193c01afa6a41db13681b880f1baa79543827
Sai <saisandhya187@gmail.com>
2023-08-12 14:08:41 +0530

    Create app home page

---
+++ CustomerApp/resources/frontend/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Credit card management system</h1>
+      <div>
+        <div>
+          <button>Transactions</button>
+        </div>
+        <div>
+          <button>Add Credit card</button>
+        </div>
+        <div>
+          <button>Delete Credit card</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
