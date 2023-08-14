bc58fb9c8b30ffdc1f0529aa3140c7a2e3827f2f
Ashwin V Pillai <ashwinpillaiofficial@gmail.com>
2023-08-14 16:01:32 +0000

    added select options

--- frontend/src/components/Transaction.js
+++ frontend/src/components/Transaction.js
@@ -1,27 +1,18 @@
 import React, { useState } from "react";
-
-const genderOptions = ["None", "Male", "Female"];
-const categoryOptions = [
-  "None",
-  "gas_transport",
-  "shopping_pos",
-  "misc_pos",
-  "shopping_net",
-];
-const professionOptions = [
-  "None",
-  "Media planner",
-  "Ambulance person",
-  "Podiatrist",
-  "Architectural technologist",
-  "Energy engineer",
-];
+import {categories,professions,merchants,city ,states} from "./filterOptions"
 
 function Transaction() {
   const [transaction, setTransaction] = useState({
-    gender: "None",
-    category: "None",
-    profession: "None",
+    pageno:0,
+    pagesize:10,
+    gender: "null",
+    category: "null",
+    merchant:"null",
+    city:"null",
+    state:"null",
+    lower:-1,
+    upper:-1,
+    profession: "null"
   });
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -45,26 +36,68 @@ function Transaction() {
   return (
     <div>
       <form onSubmit={handleSubmit}>
+      <div>
+        <p>Show</p>
+          <input name="pagesize" type="number" key={10}></input>
+          <p>entries</p>
+        </div>
         <div>
+          <p>Gender</p>
           <select name="gender" onChange={handleSelectChange}>
-            {genderOptions.map((option, index) => {
-              return <option key={index}>{option}</option>;
-            })}
+          <option value= "null">None</option>
+            <option value="M">Male</option>
+            <option value="F">Female</option>
           </select>
         </div>
         <div>
+        <p>Category</p>
           <select name="category" onChange={handleSelectChange}>
-            {categoryOptions.map((option, index) => {
+            <option key = "null">None</option>
+            {categories.map((option, index) => {
               return <option key={index}>{option}</option>;
             })}
           </select>
         </div>
         <div>
-          <select name="profession" onChange={handleSelectChange}>
-            {professionOptions.map((option, index) => {
+        <p>Merchant</p>
+          <select name="merchant" onChange={handleSelectChange}>
+            <option key = "null">None</option>
+            {merchants.map((option, index) => {
               return <option key={index}>{option}</option>;
             })}
           </select>
+        </div>
+        <div>
+        <p>City</p>
+          <select name="city" onChange={handleSelectChange}>
+            <option key = "null">None</option>
+            {city.map((option, index) => {
+              return <option key={index}>{option}</option>;
+            })}
+          </select>
+        </div>
+        <div>
+        <p>State</p>
+          <select name="state" onChange={handleSelectChange}>
+            <option key = "null">None</option>
+            {states.map((option, index) => {
+              return <option key={index}>{option}</option>;
+            })}
+          </select>
+        </div>
+        <div>
+        <p>Profession</p>
+          <select name="profession" onChange={handleSelectChange}>
+          <option key = "null">None</option>
+            {professions.map((option, index) => {
+              return <option key={index}>{option}</option>;
+            })}
+          </select>
+        </div>
+        <div>
+        <p>Limit</p>
+          <input name="lower" type="text" placeholder="Lower limit"></input>
+          <input name="upper" type="text" placeholder="Upper limit"></input>
         </div>
         <button type="submit">Search</button>
       </form>
